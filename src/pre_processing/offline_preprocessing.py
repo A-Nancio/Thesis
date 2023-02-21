43b23266ac35ca03b1a20d31d078b3b0103682de
Beatnancio <avenancio2000@hotmail.com>
2023-02-21 14:08:57 +0000

    bug fixes

--- src/pre_processing/offline_preprocessing.py
+++ src/pre_processing/offline_preprocessing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import math
-from scipy import zscore
+import numpy as np
 
 #NOTE REDUNDANT FUNCTION, CAN BE DONE IN A SINGLE LINE
 #def convert_numerical_feat(column: pd.Series, features):
@@ -11,34 +11,32 @@ from scipy import zscore
 #    return dataset
 
 #TODO function to have time delta between the last transaction
-
 def convert_bucket_feat(column: pd.Series):
     """Applies percentile bucketing to a panda Series
     """
-    #TODO TO BE TESTED
-    percentile, lower_limit = 0.01, column.min()
-    while percentile <= 1:
-        upper_limit = column.quantile(percentile)
-        
-        mask = lower_limit <= column < upper_limit
-        column.loc[mask] = (percentile-0.01) * 100
-        
-        lower_limit = upper_limit
-        percentile += 0.01
+    initial_column = column.copy()  #without it the quantiles are wrongly calculated
 
+    #NOTE verify if values are placed onto the right buckets
+    lower_quantile = 0
+    for upper_quantile in np.arange(0.01, 1.01, 0.01):
+        lower_limit = initial_column.quantile(lower_quantile)
+        upper_limit = initial_column.quantile(upper_quantile)
+        
+        column[(column >= lower_limit) & (column <= upper_limit)] = (upper_quantile-0.01) * 100
+        lower_quantile = upper_quantile
+
+    column.fillna(100)
     return column#convert_categorical_feat(dataset, features, 25) #max_index proposed is 25 since there are 100 buckets
 
 def convert_categorical_feat(column: pd.Series, max_index: int):
     """Converts all possible values of a categorical feature by mapping each
     possible value into a integer based on the number of occurrences
     """
-    #TODO BE TESTED
     occurence_list = column.value_counts()
     l = 1
     for index, value in occurence_list.items():
         column = column.replace(index, l)
-        if l < max_index:
-            l = l + 1
+        l = l + 1
 
     return column
 
@@ -50,7 +48,6 @@ def convert_time(feature_column, format: int):
     format: indicator if to convert either hour of the day, 
     day of the week or the day of the month
     """
-    #TODO BE TESTED
     sin_projection = feature_column.apply(lambda x: math.sin(x *(2 * math.pi / format)))
     cos_projection = feature_column.apply(lambda x: math.cos(x *(2 * math.pi / format)))
 
