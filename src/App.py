8587bb52181ab98b3d67ba07d767dc29fa532093
António Venâncio <antonio.venancio@tecnico.ulisboa.pt>
2023-05-27 17:34:39 +0100

    Implemented Feedzai's shared state

--- src/App.py
+++ src/App.py
@@ -1,31 +1,25 @@
 import logging
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-from data_processing.batch_generator import load_train_set
-from machine_learning.models import StatefulModel
-from machine_learning.train import train_model, test_model
+from data_processing.batch_generator import load_train_set, load_test_set
+from machine_learning.models import TrainSingleState, InferenceSingleState, BATCH_SIZE
 import tensorflow as tf
 from keras import metrics
 
 
-#model.compile(
-#    optimizer=keras.optimizers.Adam(),
-#    loss=keras.losses.BinaryCrossentropy(from_logits=True),
-#    metrics=(metrics.BinaryAccuracy(),
-#               metrics.TruePositives(), metrics.TrueNegatives(),
-#               metrics.FalsePositives(), metrics.FalseNegatives()))
 SEQUENCE_LENGTH = 100
-BATCH_SIZE = 128
-NUM_EPOCHS = 5
+NUM_EPOCHS = 20
 
-StatefulModel.compile(loss=tf.keras.losses.BinaryCrossentropy(),
-                optimizer=tf.keras.optimizers.Adam(),
-                metrics=[metrics.BinaryAccuracy(),
-               metrics.TruePositives(), metrics.TrueNegatives(),
-               metrics.FalsePositives(), metrics.FalseNegatives()])
+train_model = TrainSingleState()
 
-total = 1296675
-neg = 1289169
+train_model.compile(loss=tf.keras.losses.BinaryCrossentropy(),
+            optimizer=tf.keras.optimizers.Adam(),
+            metrics=[metrics.BinaryAccuracy(),
+                metrics.TruePositives(), metrics.TrueNegatives(),
+                metrics.FalsePositives(), metrics.FalseNegatives()])
+
+total = 1000000
+neg = 800000
 pos = 7506
 
 weight_0 = (1 / neg) * (total / 2.0)
@@ -33,31 +27,19 @@ weight_1 = (1 / pos) * (total / 2.0)
 
 class_weight = {0: weight_0, 1: weight_1}
 
-
-#ratios = ((16,16), (15,17), (14,18), (13,19), (12,20), (11,21))
-#ratios = ((28, 4),)
-
-
-#metrics = {}
-#test_set, test_labels, test_transactions = import_test_set()
-
-#for ratio in ratios:
-
+# TRAIN THE MODEL
 train_set = load_train_set(sequence_length=SEQUENCE_LENGTH, batch_size=BATCH_SIZE)
 
-StatefulModel.fit(train_set, epochs=NUM_EPOCHS, class_weight=class_weight, verbose='auto', shuffle=True)
-#for epoch in range(NUM_EPOCHS):
-#    metrics[ratios] = ()
-#    print(f"\nStart of epoch {epoch}")
-#    print("------- TRAINING LOOP -------")
-#    train_model(model, train_set)
-#    print("------- TESTING LOOP -------")
-#    validation_results = test_model(model, test_set, test_labels, test_transactions)
-#    metrics[ratios] += (validation_results,)
-#
-#    #model.reset
-#    #break
-#
-#    
-#print(metrics)
-#
+train_model.fit(train_set, epochs=NUM_EPOCHS, class_weight=class_weight, verbose='auto', shuffle=True)
+
+# MODEL IN PRODUCTION
+production_model = InferenceSingleState()
+production_model.set_weights(train_model.get_weights())
+
+test_set = load_test_set()
+production_model.evaluate(test_set)
+
+#TODO store results
+
+
+
--- src/machine_learning/models.py
+++ src/machine_learning/models.py
@@ -1,77 +1,122 @@
 """Tensorflow model and layer classes"""
 import tensorflow as tf
-from keras.layers import concatenate, Dense, GRU, Dropout
+from keras.layers import concatenate, Dense, GRU, GRUCell, Dropout, RNN
+import numpy as np
 
+CARD_ID_COLUMN = 0
+CATEOGRY_ID_COLUMN = ...
+BATCH_SIZE=128
 
-stateless_model = tf.keras.models.Sequential([
-    Dense(64, activation="relu"),
-    Dense(64, activation="relu"),
-    Dense(1, activation="sigmoid")
-])
+class SharedStateCell(GRUCell):
+    def __init__(self, 
+                 units, 
+                 id_column,
+                 activation="tanh", 
+                 recurrent_activation="hard_sigmoid", 
+                 use_bias=True, 
+                 kernel_initializer="glorot_uniform", 
+                 recurrent_initializer="orthogonal", 
+                 bias_initializer="zeros", 
+                 kernel_regularizer=None, 
+                 recurrent_regularizer=None, 
+                 bias_regularizer=None, 
+                 kernel_constraint=None, 
+                 recurrent_constraint=None, 
+                 bias_constraint=None, 
+                 dropout=0, 
+                 recurrent_dropout=0, 
+                 reset_after=False,
+                 **kwargs):
+        
+        super().__init__(units, activation, recurrent_activation, use_bias, kernel_initializer, recurrent_initializer, bias_initializer, kernel_regularizer, recurrent_regularizer, bias_regularizer, kernel_constraint, recurrent_constraint, bias_constraint, dropout, recurrent_dropout, reset_after, **kwargs)
+        self.shared_states: tf.Variable = tf.Variable(tf.zeros([1000, units]))
+        self.id_column = id_column
 
-StatefulModel = tf.keras.models.Sequential([
-    Dropout(0.2),
-    GRU(64, return_sequences=False, dropout=0.3, recurrent_regularizer='l2'),
-    Dense(1, activation="sigmoid"),
-    Dense(32, activation='relu'),
-    Dense(1,  activation="sigmoid")
-])
+    def call(self, inputs, states, training=None):
+        """
+        tensors should be of size:
+        inputs: [batch_size (BATCH_SIZE), num_features (18)]
+        states: [batch_size (BATCH_SIZE, num_units (64))]
+        """
+        ids = tf.dtypes.cast(inputs[:, self.id_column], tf.int32) # shape =
 
-class SharedState(tf.keras.Model):
-    def __init__(self):
-        super(GlobalState, self).__init__()
+        input_states= tf.gather(self.shared_states, ids)
+        output, new_states = super().call(inputs, input_states, training)
+        
+        self.shared_states.scatter_nd_update(ids, new_states)
+
+        return output, new_states
+        # NOTE How do I synchronize with batch entries where 2 entries have the same ID, #NOTE this is fine, tensorflow operations come with synchronization techniques
+
+class TrainSingleState(tf.keras.Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.card_gru = RNN(SharedStateCell(units=32, id_column=CARD_ID_COLUMN, dropout=0.3, recurrent_regularizer='l2'))
         self.dropout = Dropout(0.2)
-        self.card_gru = GRU(64, return_sequences=False,
-                       dropout=0.3, recurrent_regularizer='l2')
-        self.zip_gru = GRU(64, return_sequences=False,
-                       dropout=0.3, recurrent_regularizer='l2')
         self.dense = Dense(32, activation='relu')
+        self.out = Dense(1,  activation="sigmoid")
 
+    def call(self, inputs, training=None, mask=None):
+        var = self.card_gru(inputs)
+        var = self.dropout(var)
+        var = self.dense(var)
+        out = self.out(var)
+
+        return out
+    
+
+class DoubleStateModel(tf.keras.Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.card_gru = RNN(SharedStateCell(32, id_column=CARD_ID_COLUMN, dropout=0.3, recurrent_regularizer='l2'))
+        self.zip_gru = RNN(SharedStateCell(32, id_column=CATEOGRY_ID_COLUMN, dropout=0.3, recurrent_regularizer='l2'))
+        self.dropout = Dropout(0.2)
+        self.dense = Dense(32, activation='relu')
         self.out = Dense(1,  activation="sigmoid")
     
 
-    def call(self, inputs):
-        var = self.dropout(inputs)
-        #if inputs[...] 
-        #card_gru.change_state(inputs[new_card])
-        var = self.gru(var)
-        #var = concatenate([inputs, var], axis=1)
+    def call(self, inputs, training=None, mask=None):
+        var = self.card_cell(inputs)
+        var = self.dropout(var)
         var = self.dense(var)
         out = self.out(var)
+
         return out
-
-class CardStateful(tf.keras.Model):
-    """A model that stores an internal state relevant to each card"""
-
-    def __init__(self, transactions):
-        self.transactions = transactions
-        super(GlobalState, self).__init__()
+    
+class InferenceSingleState(tf.keras.Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.card_cell = SharedStateCell(32, id_column=CARD_ID_COLUMN, dropout=0.3, recurrent_regularizer='l2')
         self.dropout = Dropout(0.2)
-        self.gru = GRU(64, return_sequences=False,
-                       dropout=0.3, recurrent_regularizer='l2')
-        #self.zip_gru = GRU(64, return_sequences=False,
-        #               dropout=0.3, recurrent_regularizer='l2')
         self.dense = Dense(32, activation='relu')
-
         self.out = Dense(1,  activation="sigmoid")
     
-    
-    def call(self, inputs):
-        var = self.dropout(inputs)
-        #card_gru.change_state(inputs[new_card])
-        var = self.gru(var)
-        #var = concatenate([inputs, var], axis=1)
+
+    def call(self, inputs, training=None, mask=None):
+        var = self.card_cell(inputs)
+        var = self.dropout(var)
         var = self.dense(var)
         out = self.out(var)
+
+        return out
+    
+class InferenceDoubleState(tf.keras.Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.card_cell = SharedStateCell(32, id_column=CARD_ID_COLUMN, dropout=0.3, recurrent_regularizer='l2')
+        self.zip_cell = SharedStateCell(32, id_column=CATEOGRY_ID_COLUMN, dropout=0.3, recurrent_regularizer='l2')
+        self.dropout = Dropout(0.2)
+        self.dense = Dense(32, activation='relu')
+        self.out = Dense(1,  activation="sigmoid")
+    
+
+    def call(self, inputs, training=None, mask=None):
+        var1 = self.card_cell(inputs)
+        var2 = self.zip_cell(inputs)
+        var = concatenate(var, axis=0)  # FIXME verify if axis is correct
+        var = self.dropout(var)
+        var = self.dense(var)
+        out = self.out(var)
+
         return out
 
-    def train_step(self, data):
-        return
-    
-    #def test_step(self, data)
-
-# class SharedState(GlobalState):
-#    def __init__(self):
-#        super(SharedState, self).__init__()
-#
-    # TODO override training_step to change the gru state of the model
