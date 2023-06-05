42f32246ecc4ceb1536324c06e98aa8e4b36f06f
nancio <antonio.venancio@humanready.io>
2023-06-05 20:15:09 +0100

    delete dead code

--- src/data_processing/batch_generator.py
+++ src/data_processing/batch_generator.py
@@ -2,11 +2,15 @@
 import sys
 import numpy as np
 import tensorflow as tf
+from machine_learning.models import BATCH_SIZE
+
+SEQUENCE_LENGTH = 100
 
 def load_train_set(sequence_length, batch_size):
     path = 'data/train'
     transactions = np.load(f'{path}/transactions.npy')
     labels = np.load(f'{path}/all_transaction_labels.npy')
+
     return tf.keras.utils.timeseries_dataset_from_array(
         transactions,
         labels,
@@ -20,77 +24,28 @@ def load_train_set(sequence_length, batch_size):
         end_index=None
     )
 
-
 def load_test_set():
     path = 'data/test'
     dataset = np.load(f'{path}/transactions.npy')
     labels = np.load(f'{path}/all_transaction_labels.npy')
-    return tf.data.Dataset.from_tensor_slices(dataset, labels, batch_size=1)
 
+    return tf.data.Dataset.from_tensor_slices((dataset, labels)).batch(1)   # needs batch size 1 to have all sequential transactions
 
-SEQUENCE_LENGTH = 100
+def load_pre_data():
+    # Last 200 transaction from training to prepare the inference model state
+    pre_inference_data = np.load(f'data/train/transactions.npy')[-200:]   
+    pre_inference_labels = np.load(f'data/train/all_transaction_labels.npy')[-200:]
 
-def import_training_set(ratio: tuple[int, int]):
-    """Load .npy sequences from disk, generate batches and return dataset for training"""
+    return tf.data.Dataset.from_tensor_slices((pre_inference_data, pre_inference_labels)).batch(1)
 
-    path = 'data/train'
-    transactions = np.load(f'{path}/transactions.npy')
-    non_frauds = np.load(f'{path}/sequences/non_fraud_train_seq.npy')
-    frauds = np.load(f'{path}/sequences/fraud_train_seq.npy')
+def get_class_weights():
+    # fetch class weights for model to handle highly imbalanced data
+    total = 607506
+    neg = 600000
+    pos = 7506
+    # FIXME change after to original value
 
-    batch_size = ratio[0] + ratio[1]
+    weight_0 = (1 / neg) * (total / 2.0)
+    weight_1 = (1 / pos) * (total / 2.0)
 
-    batch = np.empty(shape=(0, SEQUENCE_LENGTH), dtype=np.int64)
-    dataset = np.empty(shape=(0, batch_size, SEQUENCE_LENGTH), dtype=np.int64)
-    labels = np.empty(shape=(0, batch_size), dtype=np.int64)
-    np.random.shuffle(non_frauds)
-    #np.random.shuffle(frauds)
-
-    original_frauds = np.copy(frauds)
-    num_batches = 0
-    sys.stdout.write(f"Generating batches for {ratio[0]} non fraud and {ratio[1]} fraud transactions: ")
-    sys.stdout.flush()
-
-    while len(non_frauds) > ratio[0]:
-        np.random.shuffle(frauds)
-
-        
-        if num_batches % 100 == 0:
-            print(num_batches)
-        num_batches += 1
-
-        batch = np.append(non_frauds[0:ratio[0]],
-                          frauds[0:ratio[1]], axis=0)[np.newaxis, :, :]
-        dataset = np.append(dataset, batch, axis=0)
-
-        # Remove sampled transactions
-        non_frauds = non_frauds[ratio[0]:]
-
-        # Add labels
-        batch_labels = np.append(
-            np.zeros((ratio[0],), dtype=int), np.ones((ratio[1],), dtype=int))
-        labels = np.append(labels, batch_labels[np.newaxis, :], axis=0)
-
-    print(f"\nGenerated {num_batches} batches")
-    print(
-        f"Generated train dataset with shape {dataset.shape} and labels {labels.shape}")
-    
-    return dataset, labels, transactions
-
-def import_full_training_set():
-    path = 'data/train'
-    transactions = np.load(f'{path}/transactions.npy')
-    non_frauds = np.load(f'{path}/sequences/non_fraud_train_seq.npy')
-    frauds = np.load(f'{path}/sequences/fraud_train_seq.npy')
-
-
-def import_test_set():
-    path = 'data/test'
-    dataset = np.load(f'{path}/all_test_seq.npy')[:,np.newaxis, :]
-    labels = np.load(f'{path}/all_test_seq_labels.npy')[:,np.newaxis]
-    transactions = np.load(f'{path}/transactions.npy')
-
-    print(
-        f"Generated test dataset with shape {dataset.shape} and labels {labels.shape}")
-    return dataset, labels, transactions
-
+    return {0: weight_0, 1: weight_1}
